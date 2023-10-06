726d6a10195d7cfef2e8b4fc992c4ccb6f4986ff
vikrant <notvikrant80@gmail.com>
2023-10-06 23:33:40 +0530

    connected to db

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const uri = "mongodb://127.0.0.1:27017/auth-service";
+mongoose
+  .connect(uri)
+  .then((result) => {
+    console.log("connected to mongo");
+  })
+  .catch((err) => {
+    console.log("error connecting to database", err);
+  });
 
 const PORT = 3000;
 app.listen(PORT, () => {
